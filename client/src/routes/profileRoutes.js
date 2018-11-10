548848a1849e924f50d10df2cc435276a7da1f83
Samuel Shih <samuels.shih@gmail.com>
2018-11-09 20:11:08 -0800

    Render user profile page

--- client/src/routes/profileRoutes.js
+++ client/src/routes/profileRoutes.js
@@ -11,7 +11,7 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-  res.send('Hello!!! ' + req.user.username);
+  res.render('profile', { user: req.user });
 });
 
 module.exports = router;
